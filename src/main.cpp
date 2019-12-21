6d21ebc9a5e27e22e86a2126ae1decc4891943f6
김도훈 <kimdh1004yo@gmail.com>
2019-12-21 06:51:34 +0000

    HW-2

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
-#include "namespaces.h"
-
-using namespace a;
-using namespace mycode;
+#include <iostream>
+#include <math.h>
 
 int main()
 {
-	//mycode::foo();
-	//a::foo();// Calls the "foo" function in the "mycode" namespace
-	foo();			// implies mycode::foo();
-	return 0;
+	int a, b;
+	std::cin >> a >> b;
+	
+	for (int i = a; i >= 2; i--){
+		if(a%i == 0 && b%i == 0){
+			a /= i;
+			b /= i;
+		}
+	}
+	
+	while (a != 1){
+		
+		int x = ceil ((double)b / a);
+		std::cout << "1/ " << x << ", ";
+		a *= x;
+		a -= b;
+		b *= x;
+		for (int i = a; i >= 2; i--){
+			if (a % i== 0 && b%i == 0){
+				a/=i;
+				b/=i;
+			}
+		}
+	}
+	std::cout << "1/ " << b << std::endl;
 }
