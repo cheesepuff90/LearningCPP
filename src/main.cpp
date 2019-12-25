6eb3450541a14ca8757d528b678e0293cabef95e
김도훈 <kimdh1004yo@gmail.com>
2019-12-25 12:15:24 +0000

    quickSort

--- src/main.cpp
+++ src/main.cpp
@@ -1,133 +1,59 @@
 #include<iostream>
 using namespace std;
 
-class number
+int sort(int a[], int x, int y)
 {
-	public:
-		number(int num);
-		virtual int absolute();
-	
-	private:
-		int mNum;
-};
-
-class Rational : public number
-{
-public:
-	Rational(double value);
-	Rational(int denom, int numer);
-	int getNumer();
-	int getDenom();
-	void setRational(double value);
-	virtual int absolute() override;
-	
-private:
-	int mNumer;
-	int mDenom;
-};
-
-number::number(int num)
-{
-	mNum = num;
-}
-int number::absolute ()
-{
-	if (mNum < 0)
-	{
-		return -mNum;
-	} else
-	{
-		return mNum;
-	}
-}
-
-Rational operator+(Rational& lhs, Rational& rhs)
-{
-	Rational res(lhs.getNumer()*rhs.getDenom() + rhs.getNumer()*lhs.getDenom(), lhs.getNumer() * rhs.getNumer());
-	return res;
-}
-
-ostream& operator<<(ostream& ostr, Rational& num)
-{
-	ostr << num.getDenom() << "/" << num.getNumer();
-	return ostr;
-}
-
-
-int gcd(int a, int b)
-{
-	if (a < b)
-		return gcd(b, a);
-	else if (a % b == 0)
-		return b;
-	else return gcd(b, a%b);
-}
-
-Rational::Rational(double value)
-{
-	int comp = (int) value;
-	int exp = 1;
-
-	while (comp != value)
-	{
+	int v, i, j, temp;
+	v = a[x];
+	i = x;
+	j = y+1;
+	do {
+		do{
+			i++;
+		}while (a[i] < v && i <= y);
 		
-		value *= 10;
-		comp = (int) value;
-		exp *= 10;
-	}
-
-	int g = gcd(value, exp);
-	mDenom = value / g;
-	mNumer = exp / g;
-}
-
-Rational::Rational(int denom, int numer)
-{
-	int g = gcd(denom, numer);
-	mDenom = denom / g;
-	mNumer = numer / g;
-}
-
-int Rational::getDenom()
-{
-	return mDenom;
-}
-
-int Rational::getNumer()
-{
-	return mNumer;
-}
-
-void Rational::setRational(double value)
-{
-	int comp = (int) value;
-	int exp = 1;
-
-	while (comp != value)
-	{
+		do{
+			j--;
+		}while (v < a[j]);
 		
-		value *= 10;
-		comp = (int) value;
-		exp *= 10;
+		if (i < j){
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+		}
 	}
+	while (i < j);
+	a[x] = a[j];
+	a[j] = v;
+	
+	return (j);
+}
 
-	int g = gcd(value, exp);
-	mDenom = value / g;
-	mNumer = exp / g;
+void quickSort (int a[], int x, int y)
+{
+	int j;
+	if (x < y){
+		j = sort(a, x, y);
+		quickSort(a, x, j-1);
+		quickSort(a, j+1, y);
+	}
 }
 
 int main()
 {
-	number n(-10);
+	int a[100], n, i;
+	cout << "array 개수: ";
+	cin >> n;
+	cout << "배열 숫자들:\n";
 	
-	Rational half(0.5);
-	Rational qurd(1, 4);
+	for (i = 0; i < n; i++)
+		cin >> a[i];
+	quickSort(a, 0, n-1);
 	
-	cout << n.absolute() << endl;
-	cout << half << endl;
-	cout << half.absolute() << endl;
-	// (half + qurd).printValue();
-	//Rational res = half + qurd; 
-	//cout << res << endl;
+	cout << "정렬된 숫자들: ";
+	
+	for (i = 0; i < n; i++)
+		cout << " " << a[i];
+	
 	return 0;
 }
