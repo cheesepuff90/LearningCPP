c92c69f30883ac92e194ebc85d82a732404157f9
김도훈 <kimdh1004yo@gmail.com>
2019-12-25 03:17:51 +0000

    rational

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,103 @@
-#include<stdio.h>
 #include<iostream>
 using namespace std;
 
-int main(){
+class Rational
+{
+public:
+	Rational(double value);
+	Rational(int denom, int numer);
+	int getNumer();
+	int getDenom();
+	void setRational(double value);
 	
-	int n, sum=0;
-	int a[150000];
-	bool flag=true;
-	
-	(cin>>n).get();
-	
-	for(int i=0;i<n;i++){
-		(cin>>a[i]).get();
-	}
-	
-	sum = a[0] > a[1] ? a[0] : a[1];
-	
-	for(int i = 2; i < n - 1; i++){
-		flag = a[i+1] > a[i]; 
+private:
+	int mNumer;
+	int mDenom;
+};
+
+Rational operator+(Rational& lhs, Rational& rhs)
+{
+	Rational res(lhs.getNumer()*rhs.getDenom() + rhs.getNumer()*lhs.getDenom(), lhs.getNumer() * rhs.getNumer());
+	return res;
+}
+
+ostream& operator<<(ostream& ostr, Rational& num)
+{
+	ostr << num.getDenom() << "/" << num.getNumer();
+	return ostr;
+}
+
+
+int gcd(int a, int b)
+{
+	if (a < b)
+		return gcd(b, a);
+	else if (a % b == 0)
+		return b;
+	else return gcd(b, a%b);
+}
+
+Rational::Rational(double value)
+{
+	int comp = (int) value;
+	int exp = 1;
+
+	while (comp != value)
+	{
 		
-		if(flag)
-			sum += a[i+1] - a[i];
+		value *= 10;
+		comp = (int) value;
+		exp *= 10;
 	}
+
+	int g = gcd(value, exp);
+	mDenom = value / g;
+	mNumer = exp / g;
+}
+
+Rational::Rational(int denom, int numer)
+{
+	int g = gcd(denom, numer);
+	mDenom = denom / g;
+	mNumer = numer / g;
+}
+
+int Rational::getDenom()
+{
+	return mDenom;
+}
+
+int Rational::getNumer()
+{
+	return mNumer;
+}
+
+void Rational::setRational(double value)
+{
+	int comp = (int) value;
+	int exp = 1;
+
+	while (comp != value)
+	{
+		
+		value *= 10;
+		comp = (int) value;
+		exp *= 10;
+	}
+
+	int g = gcd(value, exp);
+	mDenom = value / g;
+	mNumer = exp / g;
+}
+
+int main()
+{
+	Rational half(0.5);
+	Rational qurd(1, 4);
 	
-	cout<<sum<<endl;
+	cout << half << endl;
+	// (half + qurd).printValue();
+	Rational res = half + qurd; 
+	cout << res << endl;
+	return 0;
 }
