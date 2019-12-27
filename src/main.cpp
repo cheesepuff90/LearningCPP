3b8c66338677587754a9306742350456a949b30b
김도훈 <kimdh1004yo@gmail.com>
2019-12-27 03:50:04 +0000

    class

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,50 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
-int gcd (int a, int b)
+class Board
 {
-	int r = 1;
-	
-	while (r != 0)
-	{
-		r = a%b;
-		a = b;
-		b = r;
-	}
-	
-	return a;
+	public:
+		Board(int height, int width);
+		int getWidth();
+		int getHeight();
+		
+		void setSize(int height, int width);
+
+	private:
+		int mHeight;
+		int mWidth;
+};
+
+Board::Board(int height, int width) : mHeight(height), mWidth(width)
+{
 }
 
-int big (int a1, int a2, int b1, int b2)
+int Board::getHeight()
 {
-	if (a1*b2 > b1*a2)
-		return 1;
-	else return 0;
+	return mHeight;
+}
+
+int Board::getWidth()
+{
+	return mWidth;
+}
+
+void Board::setSize(int height, int width)
+{
+	mHeight = height;
+	mWidth = width;
 }
 
 int main()
 {
-	int num[100] = {};
-	int i, j, k = 0, x, y, n, nk;
+	Board cell(20, 30);
 	
-	cin >> n;
-	
-	for (i = 2; i <= n; i++){
-		for (j = 1; j < i; j++){
-			if (gcd(i, j)== 1){
-				num[k++] = j;
-				num[k++] = i;
-			}
-		}
-	}
-	nk = k;
-	
-	for(i = 0; i < nk; i += 2){
-		for (j = 0; j < nk - i - 2; j += 2){
-			if (big(num[j], num[j+1], num[j+2], num[j+3])){
-				x = num[j];
-				y = num[j+1];
-				num [j] = num [j+2];
-				num [j+1] = num [j+3];
-				num [j+2] = x;
-				num [j+3] = y;
-			}
-		}
-	}
-	cout << "0/1" << endl;
-	
-	for (i = 0; i < nk; i+=2){
-		cout << num [i] << "/" << num [i+1] << endl;
-	}
-	
-	cout << "1/1" << endl;
+	cout << "HEIGHT: " << cell.getHeight() << ", WIDTH: " << cell.getWidth() << endl; // HEIGHT: 20 WIDTH: 30
+
+	cout << setw(3) << "*";
+//	cout << cell << endl;
 	
 	return 0;
 }
