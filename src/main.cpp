f2a2080e9dfeaa954f8bd9e43ab8b05cf484e1bf
김도훈 <kimdh1004yo@gmail.com>
2019-12-28 08:25:32 +0000

    class object 어려운것

--- src/main.cpp
+++ src/main.cpp
@@ -3,48 +3,108 @@
 
 using namespace std;
 
-class Board
-{
+class Matrix
+{	
 	public:
-		Board(int height, int width);
-		int getWidth();
-		int getHeight();
-		
-		void setSize(int height, int width);
-
+		Matrix (int row, int col);
+		void set(int* m);
+		void get() const;
+		int getRow();
+		int getCol();
+		int getItem(int index);
+	
 	private:
-		int mHeight;
-		int mWidth;
+		int mRow;
+		int mCol;
+		int* mPtr;
+	
 };
 
-Board::Board(int height, int width) : mHeight(height), mWidth(width)
+Matrix::Matrix(int row, int col) : mRow(row), mCol(col) { mPtr = new int [row * col]; }
+
+void Matrix::set(int* m)
 {
+	for(int i = 0; i < mRow * mCol; i++)
+		mPtr[i] = m[i];
+}
+	
+void Matrix::get() const
+{
+	for(int i = 0; i < mRow * mCol; i++)
+	{
+		cout << mPtr[i] << " ";
+		if(i % mCol == mCol - 1)
+			cout << endl;
+	}
 }
 
-int Board::getHeight()
+int Matrix::getRow() { return mRow; }
+int Matrix::getCol() { return mCol; }
+int Matrix::getItem(int index) { return mPtr[index]; }
+
+ostream& operator<<(ostream& ostr, Matrix& m)
 {
-	return mHeight;
+	int row = m.getRow();
+	int col = m.getCol();
+	
+	for(int i = 0; i < row * col; i++)
+	{
+		ostr << setw(3) << m.getItem(i);
+		if(i % col == col - 1)
+			ostr << endl;
+	}
+	return ostr;
 }
 
-int Board::getWidth()
+Matrix operator+(Matrix& lhs, Matrix& rhs)
 {
-	return mWidth;
+	int row = lhs.getRow();
+	int col = lhs.getCol();
+	
+	int* input = new int [row * col];
+	
+	for(int i = 0; i < row * col; i++)
+		input[i] = lhs.getItem(i) + rhs.getItem(i);
+	
+	Matrix result(lhs.getRow(), lhs.getCol());
+	result.set(input);
+	
+	return result;
 }
 
-void Board::setSize(int height, int width)
+Matrix operator*(Matrix& lhs, Matrix& rhs)
 {
-	mHeight = height;
-	mWidth = width;
+	int rowl = lhs.getRow();
+	int coll = lhs.getCol();
+	// int rowr = rhs.getRow();
+	int colr = rhs.getCol();
+	
+	int* input = new int [rowl * colr];
+	
+	for(int i = 0; i < rowl; i++)
+		for(int j = 0; j < colr; j++)
+			for(int k = 0; k < coll; k++)
+				input[colr * i + j] += lhs.getItem(coll * i + k) * rhs.getItem(colr * k + j);
+	
+	Matrix result(lhs.getRow(), lhs.getCol());
+	result.set(input);
+	
+	return result;
 }
 
 int main()
 {
-	Board cell(20, 30);
+	Matrix mat1(3, 3), mat2(3, 3);
+	int input[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int input2[] = {1, 0, 0, 0, 1, 0, 0, 0, 1};
 	
-	cout << "HEIGHT: " << cell.getHeight() << ", WIDTH: " << cell.getWidth() << endl; // HEIGHT: 20 WIDTH: 30
-
-	cout << setw(3) << "*";
-//	cout << cell << endl;
+	mat1.set(input);
+	mat2.set(input2);
+	
+	Matrix result(3, 3);
+	result = mat1 * mat2;
+	
+	cout << result << endl;
 	
 	return 0;
 }
