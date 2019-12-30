5c818dc0a17ca9cc5eb57a5559de63865577d898
김도훈 <kimdh1004yo@gmail.com>
2019-12-30 07:21:41 +0000

    print number

--- src/main.cpp
+++ src/main.cpp
@@ -1,132 +1,58 @@
-/*#include <iostream>
-#include <iomanip>
-#include <chrono>
+#include <iostream>
 
 using namespace std;
-using namespace chrono;
 
-bool search(int item, int size, int arr[])
+int scale(int n)
 {
-	for (int i = 0; i < size; i++)
-		if (arr[i] == item)
-			return true;
-	return false;
+	return n ? 1 + scale(n / 10) : 0;
 }
 
-bool binarySearch(int item, int size, int arr[])
+int exp(int n)
 {
-	int low = 0, high = size - 1;
-	
-	while (low <= high)
-	{
-		int mid = (low + high) / 2;
-		if (item > arr[mid])
-			low = mid + 1;
-		else if (item < arr[mid])
-			high = mid - 1;
-		else 
-			return true;
-	}
-	return false;
+	return (n - 1) ? 10 * exp(n-1) : 1;
 }
 
 int main()
 {
+	const int segments[10][15] = {
+		{1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1},
+		{0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+		{1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1},
+		{1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+		{1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+		{1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1},
+		{1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+		{1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1}
+	};
 
-	int size = 2000;
-	int* arr = new int [size];
-	int* sortedArr = new int [size];
+	int input;
+	cout << "INPUT: ";
+	cin >> input;
 	
-	for(int i = 0; i< size; i++) {
-		arr[i] = rand() % (size * 5);
-		sortedArr[i] = arr[i];
+	cout << "SCALE: " << scale(input) << ", EXP: " << exp(scale(input)) << endl << endl;
+	
+	int size = scale(input);
+	int* foo = new int[size];
+	
+	for (int i = 0; i < size; i++){
+		foo[i] = input / exp(scale(input));
+		input = input % exp(scale(input));
 	}
 
-	for(int i = 0; i < size; i++)
-		for(int j = 0; j < size; j++)
-			if(sortedArr[j] > sortedArr[j+1]) {
-				int temp = sortedArr[j];
-				sortedArr[j] = sortedArr[j+1];
-				sortedArr[j+1] = temp;
+
+	for (int j = 0; j < 5; j++) {
+		for (int x = 0; x < size; x++) {
+			for (int y = (j * 3); y < (j*3 + 3); y++) {
+				if (segments[foo[x]][y] == 1)
+					cout << "●";
+				else cout << "○";
 			}
-	
-	//PRINTING
-	cout << "PLAIN ARR: ";
-	for(int i = 0; i< size; i++)
-		cout << setw(5) << arr[i];
-	cout << endl;
-	cout << "SORTED ARR: ";
-	for(int i = 0; i< size; i++)
-		cout << setw(5) << sortedArr[i];
-	cout << endl;
-	
-	// PLAIN SEARCH
-	time_point begin = steady_clock::now();
-	for(int i = 0; i < size; i++) {
-		int temp = rand() % (size * 5);
-		cout << search(temp, size, arr);
+			cout << " ";
+		}
+		cout << endl;
 	}
-	cout << endl;
-	time_point end = steady_clock::now();
-	cout << "Time difference in plain search= " << duration_cast<microseconds>(end - begin).count() << "[µs]" << endl;
-
-	// BINARY SEARCH
-	begin = steady_clock::now();
-	for(int i = 0; i < size; i++) {
-		int temp = rand() % (size * 5);
-		cout <<  binarySearch(temp, size, sortedArr);
-	}
-	cout << endl;
-	end = steady_clock::now();
-	cout << "Time difference in binary search= " << duration_cast<microseconds>(end - begin).count() << "[µs]" << endl;
 
 	return 0;
-}*/
-
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-
-void push_back(int size, int* arr, int*arr1)
-{
-	int* res = new int [size * 2];
-	
-	for (int i = 0; i < 2 * size; i++)
-		res[i] = i < size ? arr[i] : arr1[i];
-	
-	arr = res;
 }
-
-int main()
-{
-	int size = 10;
-	int size1 = 10;
-	int* arr = new int [size];
-	int* arr1 = new int [size1];
-	
-	for(int i = 0; i < size; i++)
-		arr[i] = 2 * i;
-	
-	for(int j = 0; j < size1; j++)
-		arr1[j] = j;
-	
-	push_back(size+size1, arr, arr1);
-	
-	for(int i = 0; i < size * 2; i++)
-		cout << setw(3) << arr[i];
-	// puch_front(item, arr);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
